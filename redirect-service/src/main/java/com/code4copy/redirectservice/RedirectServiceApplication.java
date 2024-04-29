8a8052f78b72a71766dff12ecfac89be7df9262a
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2024-04-29 22:19:04 +0530

    corrected folder name

--- redirect-service/src/main/java/com/code4copy/redirectservice/RedirectServiceApplication.java
+++ redirect-service/src/main/java/com/code4copy/redirectservice/RedirectServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedirectServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RedirectServiceApplication.class, args);
     }
