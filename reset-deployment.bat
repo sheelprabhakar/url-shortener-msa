e6ff26f0895a4d03465c24f1060ce32f471675d4
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2024-05-06 21:56:39 +0530

    update batch file

--- reset-deployment.bat
+++ reset-deployment.bat
@@ -1,3 +1,6 @@
+kubectl apply -f cd/ingress/nginx-ingress-deploy.yaml
+kubectl apply -f cd/ingress/ingress.yaml
+
 kubectl delete -f redirect-service/redirect-service.yaml
 kubectl apply -f redirect-service/redirect-service.yaml
 
