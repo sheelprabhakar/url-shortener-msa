621ff3c012258da945b791b9d8239e987a024555
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2024-07-21 17:30:49 +0530

    Refactored redirect service code

--- redirect-service/src/main/java/com/code4copy/redirectservice/config/ApplicationConfig.java
+++ redirect-service/src/main/java/com/code4copy/redirectservice/config/ApplicationConfig.java
@@ -1,23 +1,11 @@
 package com.code4copy.redirectservice.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.backoff.FixedBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
-
-import java.time.Duration;
 
 @Configuration
 @EnableRetry
--- redirect-service/src/main/java/com/code4copy/redirectservice/config/RedisConfig.java
+++ redirect-service/src/main/java/com/code4copy/redirectservice/config/RedisConfig.java
@@ -1,8 +1,6 @@
 package com.code4copy.redirectservice.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -15,68 +13,59 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
-    @Value("${spring.redis.database}")
-    private int database;
+  @Value("${spring.redis.database}")
+  private int database;
 
-    @Value("${spring.redis.host}")
-    private String host;
+  @Value("${spring.redis.host}")
+  private String host;
 
-    @Value("${spring.redis.port}")
-    private int port;
+  @Value("${spring.redis.port}")
+  private int port;
 
-    @Value("${spring.redis.timeout}")
-    private int timeout;
+  @Value("${spring.redis.timeout}")
+  private int timeout;
 
-    @Value("${spring.redis.password}")
-    private String password;
+  @Value("${spring.redis.password}")
+  private String password;
 
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(this.host, this.port);
-        configuration.setPassword(this.password);
-        configuration.setDatabase(this.database);
-        return new LettuceConnectionFactory(configuration);
-    }
+  @Bean
+  public LettuceConnectionFactory redisConnectionFactory() {
+    RedisStandaloneConfiguration configuration =
+        new RedisStandaloneConfiguration(this.host, this.port);
+    configuration.setPassword(this.password);
+    configuration.setDatabase(this.database);
+    return new LettuceConnectionFactory(configuration);
+  }
 
-    @Bean
-    public RedisCacheManager cacheManager(final RedisConnectionFactory connectionFactory) {
-        return RedisCacheManager.create(connectionFactory);
-    }
+  @Bean
+  public RedisCacheManager cacheManager(final RedisConnectionFactory connectionFactory) {
+    return RedisCacheManager.create(connectionFactory);
+  }
 
-    @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(60))
-                .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
-    }
+  @Bean
+  public RedisCacheConfiguration cacheConfiguration() {
+    return RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofMinutes(60))
+        .disableCachingNullValues()
+        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+            new GenericJackson2JsonRedisSerializer()));
+  }
 
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-        /*ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-
-        // redis serialize
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        jackson2JsonRedisSerializer.setObjectMapper(om);*/
-
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        StringRedisTemplate template = new StringRedisTemplate(factory);
-        template.setValueSerializer(stringRedisSerializer);
-        template.setHashKeySerializer(stringRedisSerializer);
-        template.setHashValueSerializer(stringRedisSerializer);
-        template.afterPropertiesSet();
-        return template;
-    }
+  @Bean
+  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+    StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+    StringRedisTemplate template = new StringRedisTemplate(factory);
+    template.setValueSerializer(stringRedisSerializer);
+    template.setHashKeySerializer(stringRedisSerializer);
+    template.setHashValueSerializer(stringRedisSerializer);
+    template.afterPropertiesSet();
+    return template;
+  }
 }
